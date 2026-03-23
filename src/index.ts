a278e0d15a6968126a784b56b5e4251193677f44
Gilberto <gilbertocarvalho1997@gmail.com>
2026-03-22 22:07:33 -0300

    correcao de erro

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,10 @@ const app = new Elysia()
   })
   .use(authRoutes)
   .use(postRoutes)
-  .listen(3000);
+  .listen({
+    port: process.env.PORT || 3000,
+    hostname: '0.0.0.0'
+  });
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
